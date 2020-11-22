10afff78fd68b11786f6ffff39b7b5e709b810b7
Ashraf Safarov <eseferov@gmail.com>
2020-11-22 12:14:56 +0400

    Small code formatting in HomeController.

--- 03 - Your First Application/PartyInvites/Controllers/HomeController.cs
+++ 03 - Your First Application/PartyInvites/Controllers/HomeController.cs
@@ -1,30 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using PartyInvites.Models;
-using System.Linq;
+﻿using System.Linq;
 
-namespace PartyInvites.Controllers {
-    public class HomeController : Controller {
-        
-        public IActionResult Index() {
+using Microsoft.AspNetCore.Mvc;
+
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
             return View();
         }
 
         [HttpGet]
-        public ViewResult RsvpForm() {
+        public ViewResult RsvpForm()
+        {
             return View();
         }
 
         [HttpPost]
-        public ViewResult RsvpForm(GuestResponse guestResponse) {
-            if (ModelState.IsValid) {
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
                 Repository.AddResponse(guestResponse);
                 return View("Thanks", guestResponse);
-            } else {
+            }
+            else
+            {
                 return View();
             }
         }
 
-        public ViewResult ListResponses() {
+        public ViewResult ListResponses()
+        {
             return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
