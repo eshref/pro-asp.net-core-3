86b2417876cc4d6e38bd82e53a858232febe916e
Ashraf Safarov <eseferov@gmail.com>
2021-02-10 14:14:46 +0400

    Small code formatting.

--- 07 - SportsStore/SportsSln/SportsStore/Controllers/HomeController.cs
+++ 07 - SportsStore/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -3,26 +3,33 @@ using SportsStore.Models;
 using System.Linq;
 using SportsStore.Models.ViewModels;
 
-namespace SportsStore.Controllers {
-    public class HomeController : Controller {
-        private IStoreRepository repository;
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStoreRepository _repository;
         public int PageSize = 4;
 
-        public HomeController(IStoreRepository repo) {
-            repository = repo;
+        public HomeController(IStoreRepository repo)
+        {
+            _repository = repo;
         }
 
         public ViewResult Index(int productPage = 1)
-           => View(new ProductsListViewModel {
-               Products = repository.Products
-                   .OrderBy(p => p.ProductID)
-                   .Skip((productPage - 1) * PageSize)
-                   .Take(PageSize),
-               PagingInfo = new PagingInfo {
-                   CurrentPage = productPage,
-                   ItemsPerPage = PageSize,
-                   TotalItems = repository.Products.Count()
-               }
-           });
+        {
+            return this.View(new ProductsListViewModel
+            {
+                Products = _repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = _repository.Products.Count()
+                }
+            });
+        }
     }
 }
